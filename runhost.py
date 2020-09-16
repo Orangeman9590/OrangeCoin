b260e517ffb8fe23ee7e2cf44d67ceefce30e92f
0rang3man <61301004+Orangeman9590@users.noreply.github.com>
2020-09-16 19:40:28 -0400

    Add files via upload

---
+++ runhost.py
@@ -0,0 +1,12 @@
+import pprint
+from argparse import ArgumentParser
+from routes import app
+pp = pprint.PrettyPrinter(indent=4)
+
+
+parser = ArgumentParser()
+parser.add_argument('-H', '--host', default='127.0.0.1')
+parser.add_argument('-p', '--port', default=5000, type=int)
+args = parser.parse_args()
+
+app.run(host=args.host, port=args.port, debug=True)
